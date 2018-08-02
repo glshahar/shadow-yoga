e311681b7645d9ad0289f81729c2a2ca68cb824d
גל שחר <galsh20@gmail.com>
2018-08-02 18:11:43 +0300

    first commit - shani

---
+++ js/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var appUrl = "http://localhost:3000/";
+// var appUrl = "https://karni-server.herokuapp.com/";
+
+var app = angular.module('app', []);
+
+app.controller('appCtrl', function($scope, $http) {
+
+    $scope.openNavbar = function () {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+            $scope.disableScroll = true;
+        } 
+        else if(document.getElementById("myTopnav").classList.contains("responsive")) {
+            document.getElementById("myTopnav").classList.remove("responsive"); 
+            $scope.disableScroll = false;
+        }
+
+        var x = document.getElementById("darkBox");
+        if (x.className === "darkBox") {
+            x.className += " darkBoxActive";
+        } else {
+            x.className = "darkBox";
+        }
+    };
+
+    $scope.sendContactForm = function () {
+
+        if($scope.fullName&&$scope.email&&$scope.message) {
+            $scope.contactForm = {
+                "fullName": $scope.fullName,
+                "email": $scope.email,
+                "message": $scope.message
+            }
+            // alert("$scope.contactForm: "+JSON.stringify($scope.contactForm, null, 4))
+
+            // $http({
+            //     method : "POST",
+            //     url : appUrl+"sendContactForm",
+            //     data: {form: $scope.contactForm}
+            // }).then(function (response) {
+            //     console.log("sendContactForm successfully !");
+            //     console.log("JSON: "+JSON.stringify(response.data, null, 4));
+            // }, function (response) {
+            //     console.log("sendContactForm Error !");
+            // });
+
+            $scope.fullName = "";
+            $scope.email = "";
+            $scope.message = "";
+
+        }
+    };
+    
+});
